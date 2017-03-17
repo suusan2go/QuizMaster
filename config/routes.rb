7dbf901022cf4d3c02c101c34a26a1aea287fb4e
suzan2go <ksuzuki180@gmail.com>
2017-03-18 01:14:48 +0900

    add logout endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'root#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
+    resource :session, only: [:destroy]
     resources :quizzes, only: [:index, :create, :update, :destroy] do
       resources :quiz_trials, only: [:show, :create, :update], shallow: true do
         resources :user_answers, only: [:show, :create], module: :quiz_trials
