3fe930a0d4e5efa431e8a28b24a534f1aa431f42
suzan2go <ksuzuki180@gmail.com>
2017-03-19 23:34:22 +0900

    update schema annotattion

--- spec/factories/quiz_trials.rb
+++ spec/factories/quiz_trials.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: quiz_trials
+#
+#  id          :integer          not null, primary key
+#  quiz_id     :integer          not null
+#  user_id     :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  finished_at :datetime
+#  status      :integer          default("0"), not null
+#
+# Indexes
+#
+#  index_quiz_trials_on_quiz_id  (quiz_id)
+#  index_quiz_trials_on_user_id  (user_id)
+#
+
 FactoryGirl.define do
   factory :quiz_trial do
     association :user, factory: :user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  image_url  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#  index_users_on_name   (name) UNIQUE
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
