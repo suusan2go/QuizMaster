dc824bff5e240d7f2fcc83fc58dd6b29b0b4e412
suzan2go <ksuzuki180@gmail.com>
2017-03-20 14:06:06 +0900

    fix exmaple text to uppercase because css loaded

--- spec/features/toppages_spec.rb
+++ spec/features/toppages_spec.rb
@@ -5,6 +5,6 @@ RSpec.feature 'Toppages', type: :feature, js: true do
   scenario 'User find a signin button' do
     visit '/'
     screenshot_and_save_page
-    expect(page).to have_text('Sign In With Google')
+    expect(page).to have_text('SIGN IN WITH GOOGLE')
   end
 end
